df219e549b67fa894c629426e2c258b384b3f3e6
Piyush Singh <piyushsingh8796@gmail.com>
2018-10-01 23:43:10 +0530

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,17 +7,16 @@ class QuizQuestion extends Component{
         return(
               <main>
                   <section>
-                    <p>{this.props.quiz_question.instruction_text} </p>
+                    <p>{this.props.quiz_question.instruction_text}</p>
                   </section>
                   <section className="buttons">
                       <ul>
-                        <li>
-                        {this.props.quiz_question.answer_options[0]}
-                        </li>
+                        <li>{this.props.quiz_question.answer_options[0]}</li>
                     </ul>
                     </section>
                 </main>
-        )}
+        )
+    }
 }
 
 export default QuizQuestion
