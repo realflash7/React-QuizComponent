d520382673c9afbf4db9ff861e9279ed90a68bf0
Piyush Singh <piyushsingh8796@gmail.com>
2018-10-02 09:18:09 +0530

    Update QuizEnd.js

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -5,10 +5,10 @@ class QuizEnd extends Component{
     render(){
 
         return (
-          <div>
-            <p>Thanks for Playing Duh!</p>
-            <a href=' '>Reset Quiz</a>
-          </div>
+           <div>
+        <p>Thanks for playing!</p>
+        <a href=''>Reset Quiz</a>
+      </div>
         )
     }
 }
